2b38a2293395478652cab9f45dddf0205df477b3
Anderson Hitoshi Uyekita <anderson.uyekita@gmail.com>
2015-07-25 20:03:39 -0300

    This file commited using Github

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Anderson Hitoshi Uyekita
+## R Programming
+## Programming Assignment 2
 
-## Write a short comment describing this function
+## This function creates a special "matrix"
 
 makeCacheMatrix <- function(x = matrix())
 {
         m <- NULL
-        set <- function(y) {
+        
+        #### SET ####
+        set <- function(y)
+        {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        
+        #### GET ####
+        get <- function()
+                x
+        
+        setsolve <- function(mean)
+                m <<- mean
+        
+        getsolve <- function()
+                m
+        
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
 
 cacheSolve <- function(x, ...)
 {
-        m <- x$getmean()
-        if(!is.null(m)) {
+        m <- x$getsolve()
+        
+        #### Testing the valeu of "m" ####
+        if(!is.null(m))
+        {
                 message("getting cached data")
                 return(m)
         }
+        
         data <- x$get()
+        
         m <- solve(data, ...)
-        x$setmean(m)
-        m
+        x$setsolve(m)
+        m ## return the inverse matrix
 }
-
-
-c <- matrix(c(2,1,1,1,2,1,1,1,2),nrow = 3,ncol = 3)
-cacheSolve(makeCacheMatrix(c))
-solve(c)
-
